b180e2597220f037774f0e41a4043979ee65a2e5
261196 <261196@vutbr.cz>
2026-04-23 16:22:44 +0200

    konec

--- k.py
+++ k.py
@@ -31,57 +31,163 @@ numbers = random_numbers(50, low=0, high=20)  # 5 čísel v rozsahu 0–20
 #     nums = numbers.copy()
 #     n = len(nums)
 #     for i in range(n):
+# #         for j in range(0, n - i - 1):
+# #             index_highlight1 = j
+# #             index_highlight2 = j + 1
+# #             colors = ["steelblue"] * len(values)
+# #             colors[index_highlight1] = "tomato"
+# #             colors[index_highlight2] = "tomato"
+# #             plt.clf()
+# #             plt.bar(range(len(values)), values, color=colors)
+# #             plt.title("Bubble Sort")
+# #             plt.pause(0.1)
+# #             if nums[j] > nums[j + 1]:
+# #                 nums[j], nums[j + 1] = nums[j + 1], nums[j]
+# #             plt.ioff()
+# #             plt.show()
+# #
+# #     return nums
+# # # print(bubble_sort(numbers))
+# # #
+# # help(sorted)
+#
+# import matplotlib.pyplot as plt
+#
+# plt.ion()
+#
+# def bubble_sort(numbers):
+#     nums = numbers.copy()
+#     n = len(nums)
+#     for i in range(n):
 #         for j in range(0, n - i - 1):
 #             index_highlight1 = j
 #             index_highlight2 = j + 1
-#             colors = ["steelblue"] * len(values)
+#
+#             colors = ["steelblue"] * len(nums)
 #             colors[index_highlight1] = "tomato"
 #             colors[index_highlight2] = "tomato"
+#
 #             plt.clf()
-#             plt.bar(range(len(values)), values, color=colors)
+#             plt.bar(range(len(nums)), nums, color=colors)
 #             plt.title("Bubble Sort")
 #             plt.pause(0.1)
+#
 #             if nums[j] > nums[j + 1]:
+#
 #                 nums[j], nums[j + 1] = nums[j + 1], nums[j]
-#             plt.ioff()
-#             plt.show()
 #
 #     return nums
-# # print(bubble_sort(numbers))
-# #
-# help(sorted)
+#
+# values =[7, 15, 7, 14, 20, 14, 3]
+# print(bubble_sort(values))
+#
+# plt.ioff()
+# plt.show()
+#
+# p
 
+from sorting import random_numbers
+
+
+import random
 import matplotlib.pyplot as plt
 
-plt.ion()
 
-def bubble_sort(numbers):
-    nums = numbers.copy()
-    n = len(nums)
-    for i in range(n):
-        for j in range(0, n - i - 1):
-            index_highlight1 = j
-            index_highlight2 = j + 1
+def random_numbers(count, low=0, high=100):
+    return [random.randint(low, high) for _ in range(count)]
 
-            colors = ["steelblue"] * len(nums)
-            colors[index_highlight1] = "tomato"
-            colors[index_highlight2] = "tomato"
 
-            plt.clf()
-            plt.bar(range(len(nums)), nums, color=colors)
-            plt.title("Bubble Sort")
-            plt.pause(0.1)
+class StudentsGrades:
+    def __init__(self, scores):
+        self.scores = scores
 
-            if nums[j] > nums[j + 1]:
+    def get_by_index(self, index):
+        return self.scores[index]
 
-                nums[j], nums[j + 1] = nums[j + 1], nums[j]
+    def count(self):
+        return len(self.scores)
 
-    return nums
+    def get_grade(self, index):
+        points = self.scores[index]
 
-values =[7, 15, 7, 14, 20, 14, 3]
-print(bubble_sort(values))
+        if points >= 90:
+            return "A"
+        elif points >= 80:
+            return "B"
+        elif points >= 70:
+            return "C"
+        elif points >= 60:
+            return "D"
+        elif points >= 50:
+            return "E"
+        else:
+            return "F"
 
-plt.ioff()
-plt.show()
+    def find(self, value):
+        result = []
 
-p
+        for i in range(len(self.scores)):
+            if self.scores[i] == value:
+                result.append(i)
+
+        return result
+
+    def get_sorted(self):
+        nums = self.scores.copy()
+        n = len(nums)
+
+        for i in range(n):
+            for j in range(0, n - i - 1):
+                if nums[j] > nums[j + 1]:
+                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
+
+        return nums
+
+
+def main():
+
+    # kolik studentů
+    results = StudentsGrades([85, 42, 91, 67, 50, 73, 100, 38, 58])
+    print("Pocet studentu:", results.count())
+
+    # vypis studentu
+    for i in range(len(results.scores)):
+        score = results.scores[i]
+        grade = results.get_grade(i)
+        print("Student", i, ":", score, "points -", grade)
+
+    # kdo měl 100
+    print("Studenti s 100 body:")
+    print(results.find(100))
+
+    # seřazení
+    print("Serazene vysledky:", results.get_sorted())
+
+    # random data
+    random_scores = random_numbers(50, 0, 100)
+    random_results = StudentsGrades(random_scores)
+
+    print("\nRandom data")
+
+    # kolik studentů
+    print("Pocet studentu:", random_results.count())
+
+    # vypis studentu
+    for i in range(len(random_results.scores)):
+        score = random_results.scores[i]
+        grade = random_results.get_grade(i)
+        print("Student", i, ":", score, "points -", grade)
+
+    # seřazení
+    print("Serazene:", random_results.get_sorted())
+
+    # graf
+    plt.hist(random_scores, bins=10, edgecolor="black")
+    plt.title("Rozdělení bodů")
+    plt.xlabel("Body")
+    plt.ylabel("Počet studentů")
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
